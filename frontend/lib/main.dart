f07c71c3d9d71d5f81f9937331829f224934e6cf
Venkatesh <23mh1a0412@acoe.edu.in>
2026-09-10 21:49:18 +0530

    feat(frontend): integrate device_preview package into main.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/app_theme.dart';
 import 'features/home/home_screen.dart';
-
 import 'features/auth/auth_provider.dart';
 
 void main() {
-  runApp(const ProviderScope(child: ATSChatbotApp()));
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const ProviderScope(child: ATSChatbotApp()),
+    ),
+  );
 }
 
 class ATSChatbotApp extends ConsumerWidget {
@@ -19,6 +25,9 @@ class ATSChatbotApp extends ConsumerWidget {
     return MaterialApp(
       title: 'ATS Resume Matcher',
       debugShowCheckedModeBanner: false,
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       theme: AppTheme.darkTheme,
       home: const HomeScreen(),
     );
